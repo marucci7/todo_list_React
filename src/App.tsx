6e0e73e2c7f02fec58e847249a2e3fea19acabea
gabriel marucci <gabrielmarucci777@hotmail.com>
2023-09-08 08:50:19 -0300

    todo list enviando para deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+
+// components
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+
+import TaksList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
+import Modal from "./components/Modal";
+
+// interfaces
+import { ITask } from "./interfaces/Task";
+
+// css 
+import styles from "./App.module.css";
+
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deletTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal")
+    if(display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updateTask: ITask = {id, title, difficulty}
+    
+    const updateItens = taskList.map((task) => {
+      return task.id === updateTask.id ? updateTask : task
+    })
+
+    setTaskList(updateItens);
+    hideOrShowModal(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Modal children={<TaskForm  btnText="Editar Tarefa" taskList={taskList} task={taskToUpdate} handleUpdate={updateTask} /> }  />
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar tarefa" taskList={taskList} setTaskList={setTaskList}  />
+        </div>
+        <div>
+          <h2>Suas tarefas: </h2>
+          <TaksList taskList={taskList} handleDelete={deletTask} handleEdit={editTask}  />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
